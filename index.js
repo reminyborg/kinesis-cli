a256b8e16b5b4e9c0b547a6c8b00a11b808d777c
Remi Nyborg <remi@nyborg.priv.no>
2016-04-27 01:14:39 +0200

    Split lines in packages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-var split = require('split')
+var split = require('split2')
 var Transform = require('stream').Transform
 var kinesis = require('kinesis')
 
@@ -8,13 +8,21 @@ var argv = require('minimist')(process.argv.slice(2))
 
 var stream = kinesis.stream({name: argv._[0], oldest: argv.o })
 
-var unpack = new Transform({objectMode: true})
-unpack._transform = function(record, _, next) {
-  next(null, record.Data.toString('ascii') + '\n')
-}
+var unpack = new Transform({
+  objectMode: true,
+  transform: function(record, _, next) {
+    next(null, record.Data.toString('ascii') + '\n')
+  }
+})
+
+var pack = new Transform({
+  transform: function (line, _, next) {
+    next(null, line)
+  }
+})
 
 if (process.stdin.isTTY) {
   stream.pipe(unpack).pipe(process.stdout)
 } else {
-  process.stdin.pipe(split()).pipe(stream)
+  process.stdin.pipe(split()).pipe(pack).pipe(stream)
 }
